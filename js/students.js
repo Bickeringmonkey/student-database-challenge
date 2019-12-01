bc28c8e1e456be598603dcf59ac03b6264124f00
Bickeringmonkey <smeadows110280@gmail.com>
2019-12-01 18:32:01 +0000

    commit

---
+++ js/students.js
@@ -0,0 +1,8 @@
+var students = [
+    {name : 'Barbara', track : 'Excel', achievements : 56, points : 21056},
+    {name : 'Steve', track : 'Front End Web Design', achievements : 89, points : 11056},
+    {name : 'Fede', track : 'Front End Web Design', achievements : 28, points : 1056},
+    {name : 'Jesus', track : 'IOS', achievements : 5, points : 56},
+    {name : 'Fati', track : 'Law', achievements : 156, points : 1056}
+];
+
